4e3aeef5bf72089c432db895415ed43b886742ea
shiyiwen <shiyiwen@sunteng.com>
2018-07-25 10:15:44 +0800

    add ipvs mode

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # kubernetes 一些部署脚本
 
+支持Centos7.X 以上系统,有任何问题 欢迎反馈 907765003@qq.com
--- install_master.sh
+++ install_master.sh
@@ -1,23 +1,46 @@
 #!/bin/sh
-
-h1() { printf "$(tput bold)%s\n" "$@" 
+#Author: shiyiwen
+#init kubernetes master server
+h1() { printf "$(tput bold)%s...\n$(tput sgr0)" "$@" 
+}
+h2() { ntf "$(tput setaf 3)%s\n$(tput sgr0)" "$@"
 }
 
-seccess() { printf "$(tput setaf 76)✔ %s \n" "$@"
+success() { printf "$(tput setaf 2; tput bold)✔ %s \n$(tput sgr0)" "$@"
 }
-
+error() { printf "$(tput setaf 1; tput bold) %s \n$(tput sgr0)" "$@"
+exit 0
+}
 ret() {
 if [ $? -eq 0 ]; then
-    seccess "Sucess"
+    success $"Sucess"
 else
     printf '\n'$(tput setaf 1; tput setab 0; tput bold)' Error '$(tput sgr0)'\n'
+    exit 0
 fi
 }
 printf '\n'$(tput setaf 1; tput setab 0; tput bold)'此脚本应用于系统安装之后的master节点部署'$(tput sgr0)'\n\n'
 
 sleep 2
 
+h1 $" System checking "
+h2 $"    pull google registry k8s.gcr.io"
+    curl --connect-timeout 3 -m 10 k8s.gcr.io
+ret
+h2 $"    It is Centos7.x ?"
+     [[ "`cat /etc/redhat-release|sed -r 's/.* ([0-9]+)\..*/\1/'`" == "7" ]] && success $"OK!" || error $"Error please check system version"
+h2 $"  Firewalld and iptables stop  "
+      echo "checking firewalld"
+      systemctl status firewalld
+      ret
+      echo "checking iptables"
+      systemctl stop iptables
+      ret
+echo -n "Do you want to continue [Y/N]?"
+read  answer
+[[ "$answer" == "y" || "$answer" == "Y" ]] && h1 $"Starting install..." || error $"Exit"
 
+h1 "  Change kubernetes repo"
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -28,13 +51,15 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 setenforce 0
+swapoff -a
 cat <<EOF >  /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
+vm.swappiness=0
 EOF
 sysctl --system > /dev/null
 
-h1 " (1) install docker and change docker strong dir"
+h1 $"  install docker and change docker strong dir"
 mkdir -p /data/docker
 yum install -y docker > /dev/null
 cat <<EOF > /usr/lib/systemd/system/docker.service
@@ -81,13 +106,22 @@ KillMode=process
 [Install]
 WantedBy=multi-user.target
 EOF
+h1 $"  loading ipvs kernel model"
+modprobe ip_vs_rr
+modprobe ip_vs_wrr
+modprobe ip_vs_sh
+modprobe nf_conntrack_ipv4
+modprobe ip_vs
+ret
 
-#h1 " install kubelet、kubeadm、kubectl "
-#yum install -y kubelet kubeadm kubectl
-#sed -i "s/cgroup-driver=systemd/cgroup-driver=cgroupfs/g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+h1 $" install kubelet、kubeadm、kubectl "
+yum install -y kubelet kubeadm kubectl > /dev/null
+ret
+
+h1 $" start docker and kubelet"
 systemctl daemon-reload
 
-systemctl enable docker && systemctl start docker > /dev/null
+systemctl enable docker && systemctl start docker && systemctl enable kubelet && systemctl restart kubelet
 ret
-#systemctl restart kubelet
-#ret
+
+success $"Master node initing Sucess"
